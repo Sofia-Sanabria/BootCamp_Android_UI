38bef8b756783a8520faf3b8425a1c177b31c19e
Sofia Sanabria <smse2003@gmail.com>
2025-06-23 18:15:18 -0300

    Second Commit

--- App_Games_Android/app/src/main/java/com/example/app_games_android/MainActivity.kt
+++ App_Games_Android/app/src/main/java/com/example/app_games_android/MainActivity.kt
@@ -5,12 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.app_games_android.ui.login.ui.RegistroScreen
+import com.example.app_games_android.ui.login.ui.RegistroViewModel
 import com.example.app_games_android.ui.theme.App_Games_AndroidTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +21,21 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             App_Games_AndroidTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    RegistroScreen(RegistroViewModel())
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     App_Games_AndroidTheme {
-        Greeting("Android")
+        RegistroScreen(RegistroViewModel())
     }
 }
---
+++ App_Games_Android/app/src/main/java/com/example/app_games_android/ui/login/ui/RegistroViewModel.kt
@@ -0,0 +1,62 @@
+package com.example.app_games_android.ui.login.ui
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import android.util.Patterns
+import kotlin.time.Duration.Companion.seconds
+import kotlinx.coroutines.delay
+
+
+class RegistroViewModel: ViewModel() {
+
+    // Crear LiveData para manejar TextFiel
+    private val _nombre = MutableLiveData<String>()
+    val nombre: LiveData<String> = _nombre // ':' siginifica, de tipo ...
+    private val _email = MutableLiveData<String>()
+    val email: LiveData<String> = _email
+    private val _password = MutableLiveData<String>()
+    val password: LiveData<String> = _password
+    private val _repeatPass = MutableLiveData<String>()
+    val repeatPass: LiveData<String> = _repeatPass
+
+    // Valor que va habilitar o dehabilitar el boton de Registro
+    private val _registroEnable = MutableLiveData<Boolean>()
+    val registroEnable: LiveData<Boolean> = _registroEnable
+
+    // Valor de carga de pagina
+    private val _isLoading  = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
+
+
+    fun onRegistroChanged(nombre: String, email: String, password: String, repeatPass: String) {
+        _nombre.value = nombre
+        _email.value = email
+        _password.value = password
+        _repeatPass.value = repeatPass
+
+        _registroEnable.value =
+            isValidNombre(nombre)
+            && isValidEmail(email)
+            && isValidPassword(password)
+            && isValidRepeatPass(repeatPass)
+
+    }
+
+    // Funcion para validar un nombre
+    fun isValidNombre(nombre: String): Boolean = nombre.isNotBlank() && nombre.length >= 2
+
+    fun isValidEmail(email: String): Boolean = Patterns.EMAIL_ADDRESS.matcher(email).matches()
+
+    fun isValidPassword(password: String): Boolean = password.length > 6
+
+    fun isValidRepeatPass(repeatPass: String) : Boolean = repeatPass.length > 6
+
+    // Una funcion corrutina (Sigue un hilo)
+    suspend fun onRegistroSelected() {
+        _isLoading.value = true
+        delay(4.seconds)
+        _isLoading.value = false
+    }
+
+}
